b99c27bfc5ac5b6500a7b20d449f0883136f8646
Joshua van Rijswijk <jvr2022beta@gmail.com>
2026-03-09 16:18:10 +0100

    Refactor Tailwind color safelist to use regex patterns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ export default {
     './stories/**/*.{ts,tsx,js,jsx,mdx}',
     './.storybook/**/*.{ts,tsx,js,jsx}',
   ],
+  safelist: [
+    {
+      pattern:
+        /^(bg|text|border|border-b|shadow)-[a-z]+-(50|100|200|300|400|500|600|700|800|900|950)$/,
+      variants: ['hover', 'peer-checked', 'peer-indeterminate'],
+    },
+    {
+      pattern: /^ring-[a-z]+-(50|100|200|300|400|500|600|700|800|900|950)\/(40|45)$/,
+      variants: ['focus-visible', 'focus-within', 'peer-focus-visible'],
+    },
+    {
+      pattern: /^bg-[a-z]+-(50|100|200|300|400|500|600|700|800|900|950)\/15$/,
+    },
+  ],
   theme: {},
   plugins: [],
 } satisfies Config;
